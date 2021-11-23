4dae2d15a59ca4f092d7cd58090ce4dd24ab7764
Renato Delboni <renatonato_jr@hotmail.com>
2021-11-23 17:43:54 +0900

    Changed some files
    JS still not working

--- app/javascript/components/drawer_card.html.js
+++ app/javascript/components/drawer_card.html.js
@@ -1,6 +1,8 @@
 const slideCard = () => {
   document.querySelector(".item-drawer-button")
-  .addEventListener("click", (event) => {
+  .addEventListener("click", () => {
     document.querySelector(".item-drawer").classList.toggle("active");
   });
 }
+
+export { slideCard }
