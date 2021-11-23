1e0bc1a3fe656e3712f09d5df54a695cd7307e46
David Mambou <david.mambou@gmail.com>
2021-11-23 12:02:10 +0900

    add cloudinary and one photo for exercise and station

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,5 @@
 class Exercise < ApplicationRecord
   belongs_to :station
   has_many :workouts, through: :workout_sets
+  has_one_attached :photo
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,4 @@
 class Station < ApplicationRecord
   has_many :exercises
+  has_one_attached :photo
 end
