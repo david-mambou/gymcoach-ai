f03ae8d7c2f6c00fd50b2ed608618bd8917709d8
Jesse Alvarado <contact@jessealvarado.com>
2021-11-24 18:02:54 +0900

    reconstruct partials for new structure to support flexible message types

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -32,7 +32,26 @@ class WorkoutsController < ApplicationController
     redirect_to dashboard_path
   end
 
-  # private
+  private
+
+  def ask_gpt3
+    @params = params
+    if params[:question].present?
+      client = OpenAI::Client.new
+      client.files.upload(parameters: { file: 'db/data.jsonl', purpose: 'search' })
+      response = client.answers(parameters: {
+        documents: ["I bench press 3 sets of 25kg at 8 reps on july 7th, 2020 and it was easy.", "I barbell squat 20kg twice, but failed on the third set on november 1st, 2021. But it was hard."],
+        question: params[:question],
+        model: "davinci",
+        examples_context: "In 2017, U.S. life expectancy was 78.6 years.",
+        examples: [["What is human life expectancy in the United States?","78 years."]],
+        max_rerank: 10,
+        max_tokens: 5
+      })
+     @reply = response['answers']
+   end
+  end
+
 
   # def sanitized_params
   #   #todo
