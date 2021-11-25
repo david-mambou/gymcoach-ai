8ae26f4c560ea883d60ae02d1f0b79f4d81caad8
Michael Kubota <mchivas@mbm.nifty.com>
2021-11-25 12:08:47 +0900

    form functionality fixed, ajax set,  and validations to prevent negative numbers

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -2,23 +2,23 @@ class WorkoutSetsController < ApplicationController
   def update
     @workout_set = WorkoutSet.find(params[:id])
     authorize @workout_set
-    if @workout_set.update(sanitized_params)
-      redirect_to workout_path(@workout_set.workout)
-    else
-      @workout = @workout_set.workout
-      render "workouts/show"
-    end
-    # unable to get stimulus/ajax to work on submission action, so commented out
-    # respond_to do |format|
-    #   format.html { redirect_to workout_path(@workout_set.workout)}
-    #   format.text { render partial: "workouts/workout_set", locals: { workout: @workout_set.workout, workout_set: @workout_set }, formats: [:html] }
+    @workout_set.update(sanitized_params)
+    #   redirect_to workout_path(@workout_set.workout)
+    # else
+    #   @workout = @workout_set.workout
+    #   render "workouts/show"
     # end
+    # unable to get stimulus/ajax to work on submission action, so commented out
+    respond_to do |format|
+      format.html { redirect_to workout_path(@workout_set.workout)}
+      format.text { render partial: "workouts/workout_set", locals: { workout: @workout_set.workout, workout_set: @workout_set }, formats: [:html] }
+    end
     #todo
   end
 
   private
 
   def sanitized_params
-    params.require("workout_set").permit(:nb_of_reps, :weight, :completed)
+    params.require("workout_set").permit(:nb_of_reps, :weight, :difficulty, :completed)
   end
 end
--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -2,5 +2,5 @@ class WorkoutSet < ApplicationRecord
   belongs_to :exercise
   belongs_to :workout
   enum difficulty: { easy: 0, moderate: 1, difficult: 2 }
-  validates :nb_of_reps, :weight, numericality: {greater_than_or_equal_to: 0}
+  validates :nb_of_reps, :weight, numericality: { greater_than_or_equal_to: 0}
 end
