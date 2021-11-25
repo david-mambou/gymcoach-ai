7745273c0ef37466c350bbc916595d0a93290b8b
Renato Delboni <renatonato_jr@hotmail.com>
2021-11-25 12:14:52 +0900

    moved body.html to home
    added icons to home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_130702) do
-  
+ActiveRecord::Schema.define(version: 2021_11_25_012247) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,13 +102,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_130702) do
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
-  end
-
-  create_table "transmissions", force: :cascade do |t|
-    t.integer "category"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
