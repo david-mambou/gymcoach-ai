c31389f229659e8a53b965ff8f11a90dd93d8983
David Mambou <david.mambou@gmail.com>
2021-11-25 22:27:13 +0900

    resolve all conflicts

--- app/helpers/ai_helper.rb
+++ app/helpers/ai_helper.rb
@@ -28,24 +28,6 @@ module AiHelper
       end
     end
 
-<<<<<<< HEAD
-    # request AI to generate a new workout card based on user requests
-    def ai_new_workout(user_message_content)
-      # double check that the message is not empty
-      if user_message_content.present?
-        client = OpenAI::Client.new
-        client.files.upload(parameters: { file: 'db/data.jsonl', purpose: 'search' })
-        response = client.answers(parameters: {
-          documents: ["I bench press 3 sets of 25kg at 8 reps on july 7th, 2020 and it was easy.", "I barbell squat 20kg twice, but failed on the third set on november 1st, 2021. But it was hard."],
-          question: user_message_content,
-          model: "davinci",
-          examples_context: "In 2017, U.S. life expectancy was 78.6 years.",
-          examples: [["What is human life expectancy in the United States?","78 years."]],
-          max_rerank: 10,
-          max_tokens: 5,
-          stop: ['\n', '=']
-          })
-=======
   # request AI to generate a new workout card based on user requests
   def ai_new_workout(user_message_content)
     if user_message_content.present?
@@ -60,7 +42,6 @@ module AiHelper
        max_rerank: 10,
        max_tokens: 5
      })
->>>>>>> 9d899bd3a87cfa2f0537fdfe646b60b292622305
 
           ai_hash = JSON.parse response.to_s
           ai_hash["answers"].each do |answer|
@@ -73,38 +54,6 @@ module AiHelper
           end
         end
 
-<<<<<<< HEAD
-        # request AI to get top X exercises based on user needs
-        def ai_top_exercises
-          # double check that the message is not empty
-          if true
-            arr = []
-            exercises = Exercise.all.select([:id, :name])
-
-            exercises.each do |exercise|
-              arr << "the #{exercise.name} exercise works on muscle groups #{exercise.muscle_list}"
-            end
-
-            client = OpenAI::Client.new
-            response = client.answers(parameters: {
-              documents: arr,
-              question: "What are some good exercises to work on the back?",
-              model: "davinci", #babbage
-              examples_context: "Bench press and pushups are good for the upper body. Lunges and squats are great for the legs.",
-              examples: [
-                ["What are the 2 top upper body exercises?", "bench press, pushups"],
-                ["What are some good leg exercises?", "lunges, squats"]
-              ],
-              max_tokens: 20,
-              stop: ['\n', '=']
-              })
-
-              reply = JSON.parse response.to_s
-              reply = reply["answers"]
-              return reply
-     end
-   end
-=======
    # request AI to get top X exercises based on user needs
    def ai_find_muscles_for_exercise(user_query)
     if true
@@ -204,5 +153,4 @@ module AiHelper
     raise
     return reply
   end
->>>>>>> 9d899bd3a87cfa2f0537fdfe646b60b292622305
 end
