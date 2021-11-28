dd45b5a4872e49519e0e4c1b4529300d7acb97c3
David Mambou <david.mambou@gmail.com>
2021-11-28 23:02:52 +0900

    exercise info is a modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initExerciseModal } from '../plugins/exercise-modal';
 
 
 // import { slideCard } from "../components/drawer_card" //Custom JS imported by Renato 23/11
@@ -30,6 +31,7 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initExerciseModal();
 
   // slideCard();
 });
