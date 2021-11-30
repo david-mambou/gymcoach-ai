d51b41f24ced7639add26d6540ffc25606adda63
David Mambou <david.mambou@gmail.com>
2021-11-30 14:20:03 +0900

    simple form second attempt cancelled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'simple_form_extension'
 
 Rails.start()
 Turbolinks.start()
