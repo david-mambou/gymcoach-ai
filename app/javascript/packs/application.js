690e2646975f2f7a858c53110078334f3008bd2a
Renato Delboni <renatonato_jr@hotmail.com>
2021-11-30 22:19:15 +0900

    added calendar js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initExerciseModal } from '../plugins/exercise-modal';
+import { calendar } from '../components/calendar';
 
 
 // import { slideCard } from "../components/drawer_card" //Custom JS imported by Renato 23/11
@@ -33,7 +34,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initExerciseModal();
-
+  renderCalendar();
   // slideCard();
 });
 
