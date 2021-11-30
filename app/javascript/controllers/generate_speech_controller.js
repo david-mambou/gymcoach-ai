f0f69d61f1db8dc8bfb699e81e520a1c560825f2
Michael Kubota <mchivas@mbm.nifty.com>
2021-11-30 14:30:40 +0900

    should work now

--- app/javascript/controllers/generate_speech_controller.js
+++ app/javascript/controllers/generate_speech_controller.js
@@ -4,16 +4,23 @@ export default class extends Controller {
   static targets = ['speech'];
 
   connect() {
+    this.speak();
+  }
+
+  speak() {
     const synth = window.speechSynthesis;
     if (this.element.dataset.spoken === "false") {
       const textString = this.speechTarget.textContent;
       const convertedTextString = new SpeechSynthesisUtterance(textString);
+      convertedTextString.lang = "en-GB";
+      const femaleVoices = ["Google UK English Female"]
+      const foundVoice = speechSynthesis.getVoices().find(({ name }) => femaleVoices.includes(name));
+      convertedTextString.volume = 1;
+      if (foundVoice) {
+        utter.voice = foundVoice;
+      }
       synth.speak(convertedTextString);
       this.element.dataset.spoken = "true";
     }
-  }
-
-  speak() {
-
   }
 }
