2b200cc9ec65d419544a80de30d70f54b95e967c
Michael Kubota <mchivas@mbm.nifty.com>
2021-11-30 15:57:56 +0900

    bleh

--- app/javascript/controllers/generate_speech_controller.js
+++ app/javascript/controllers/generate_speech_controller.js
@@ -12,8 +12,9 @@ export default class extends Controller {
     if (this.element.dataset.spoken === "false") {
       const textString = this.speechTarget.textContent;
       const convertedTextString = new SpeechSynthesisUtterance(textString);
-      convertedTextString.lang = "en-GB";
-      const femaleVoices = ["Google UK English Female", "Google US English", "Google UK English Male"];
+      convertedTextString.lang = "en-US";
+      console.log(speechSynthesis.getVoices());
+      const femaleVoices = ["Google UK English Female", "Google US English", "Google UK English Male", "en-AU"]
       const foundVoice = speechSynthesis.getVoices().find(({ name }) => femaleVoices.includes(name));
       convertedTextString.volume = 1;
       if (foundVoice) {
