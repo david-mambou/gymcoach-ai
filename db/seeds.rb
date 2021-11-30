e3672e97430b566100040f29e912b6bd5b0fc71f
Jesse Alvarado <contact@jessealvarado.com>
2021-11-30 23:33:24 +0900

    merge with michaels csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-p "clearing database.."
-User.destroy_all
-jesse = User.new(
-  email: 'jesse@lewagon.com', 
-  password: 'password123!',
-  password_confirmation: 'password123!',
-  admin: true,
-  goal: 'increase size in my chest, shoulders, and biceps',
-  age: 30,
-  name: 'Jesse',
-  routine: 'legs,chest,push'
-)
-jesse.save!
+require "open-uri"
+require "csv"
 
-Workout.destroy_all
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+station_filepath = "lib/csv_folder_for_presentation_seeds/simplified_station.csv"
+exercise_filepath = "lib/csv_folder_for_presentation_seeds/simplified_exercise.csv"
+workout_set_filepath = "lib/csv_folder_for_presentation_seeds/simplified_workoutset.csv"
+
+###############################################################################################
+# create users
+p "preparing admin credentials"
+User.destroy_all
+psw = "pass123"
+goal1 = "Increase chest size"
+rtn = 'legs,chest,push'
+jesse = User.create!(email: 'jesse@lewagon.com', name: 'Jesse', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
+david = User.create!(email: 'david@lewagon.com', name: 'david', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
+michael = User.create!(email: 'michael@lewagon.com', name: 'michael', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
+renato = User.create!(email: 'renato@lewagon.com', name: 'renato', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
+
+###############################################################################################
+# clear database
+p "clearing database.."
 Workout.destroy_all
 WorkoutSet.destroy_all
 Exercise.destroy_all
@@ -28,25 +29,7 @@ Message.destroy_all
 WorkoutTemplate.destroy_all
 p "database cleared.."
 
-  # Station.create(name: 'name',
-  #
-
-# static arrays
-MUSCLES = ['hamstrings', 'glutes', 'pecs', 'deltoids', 'quads', 'calves', 'biceps', 'erector spinae' 'triceps', 'forearms', 'shoulders', 'traps', 'abs', 'obliques', 'trapezius', 'lats', 'glutes'].freeze
-STATION_NAMES = ["captains chair", "back extension machine", "incline ab bench", "ab wheel", "seated ab machine", "bench rope machine", "rack", "open space", "leg extension machine", "lying leg curl machine", "sitting calf raise machine", "pulldown machine", "pullups bar", "dumbbells", "barbell", "fly machine", "decline bench machine", "assisted rack", "preacher curl"].freeze
-EXERCISES = ['wood-chops', 'planks', 'pushups', 'lunge', 'sit-ups', 'leg-raises', 'chinups', 'pullups', 'dips'].freeze
-MENTAL_STATE = ['tired','hungry', 'hungover','Long Rest Before', 'Good sleep', 'headache', 'knee pain', 'elbow pain', 'good energy', 'low energy'].freeze
-
-def gen_random_workout_set(workout, exercise)
-  p "creating workout set.."
-  new_set = WorkoutSet.new(order_index: 1, nb_of_reps: rand(5..12), weight: rand(5..20), difficulty: rand(1..2))
-  new_set.exercise = exercise
-  new_set.workout = workout
-  new_set.save!
-end
-
 ###############################################################################################
-
 # workout templates
 p 'generating workout templates..'
 WorkoutTemplate.create!(
@@ -76,38 +59,43 @@ WorkoutTemplate.create!(
 p "Created #{WorkoutTemplate.count} workout templates"
 
 ###############################################################################################
-
 # stations
+# STATION_NAMES = ["captains chair", "back extension machine", "incline ab bench", "ab wheel", "seated ab machine", "bench rope machine", "rack", "open space", "leg extension machine", "lying leg curl machine", "sitting calf raise machine", "pulldown machine", "pullups bar", "dumbbells", "barbell", "fly machine", "decline bench machine", "assisted rack", "preacher curl"].freeze
+
 p "creating stations.."
-20.times do
-  Station.create!(base_incremental_weight: [0.5, 1.0, 1.5, 2.0, 2.5, 4, 5].sample,
-                  name: STATION_NAMES.sample)
-                  # good_for: 'hamstrings',
-                  # bad_for: 'glutes')
+CSV.foreach(station_filepath, csv_options).each do |row|
+  Station.create!(name: row["Name"], base_incremental_weight: row["Base incremental weight"].to_i)
 end
 p "created #{Station.count} stations"
 
 ###############################################################################################
-
 # exercises
+# EXERCISES = ['wood-chops', 'planks', 'pushups', 'lunge', 'sit-ups', 'leg-raises', 'chinups', 'pullups', 'dips'].freeze
+
 p "creating exercises.."
-20.times do
-  # array = []
-  # array.push(MUSCLES.sample)
-  # new_exercise =
-  Exercise.create!(
-                  name: EXERCISES.sample,
-                  station: Station.all.sample,
-                  muscle_list: MUSCLES.sample
-                  )
-  # puts new_exercise
-  # new_exercise.tag_list.add(MUSCLES.sample)
-  # new_exercise.save!
+CSV.foreach(exercise_filepath, csv_options).each do |row|
+  new_exercise = Exercise.create!(name: row["Name"], muscle_list: row["Muscle group"], user_notes: row["user_notes"], good_for: row["Good for"], bad_for: row["Bad for"], station: Station.find_by(name: row["Station"]))
+  file = URI.open("#{row["Photos"]}")
+  p 'attaching photo to exercise..'
+  new_exercise.photo.attach(io: file, filename: 'filler.png', content_type: 'image/png')
 end
 p "created #{Exercise.count} exercises"
 
 ###############################################################################################
+# prep workout set generation
+MUSCLES = ['hamstrings', 'glutes', 'pecs', 'deltoids', 'quads', 'calves', 'biceps', 'erector spinae' 'triceps', 'forearms', 'shoulders', 'traps', 'abs', 'obliques', 'trapezius', 'lats', 'glutes'].freeze
+MENTAL_STATE = ['tired','hungry', 'hungover','Long Rest Before', 'Good sleep', 'headache', 'knee pain', 'elbow pain', 'good energy', 'low energy'].freeze
 
+def gen_random_workout_set(workout, exercise)
+  p "creating workout set.."
+  new_set = WorkoutSet.new(order_index: 1, nb_of_reps: rand(5..12), weight: rand(5..20), difficulty: rand(1..2))
+  new_set.exercise = exercise
+  new_set.workout = workout
+  new_set.save!
+end
+
+###############################################################################################
+# workouts
 p 'creating workouts'
 40.times do
   specific_workout = Workout.new(name: "Push Day", mental_state: MENTAL_STATE.sample, day: Date.today + rand(-150..15))
@@ -128,16 +116,3 @@ p 'creating workouts'
     end
   end
 end
-
-###############################################################################################
-
-# create users
-# User.all.each do |user|
-#   rand(2..10).times do
-#     # create past workout
-#     workout = Workout.first.clone
-#     workout.user = user
-#     workout.day = Date.now
-#     workout.mental_state = %w[motivated tired hungry heartbroken pumped].sample
-#   end
-# end
