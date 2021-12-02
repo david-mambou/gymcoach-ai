721edb6eb47a35ba0ec93a1775c34e10155ab3f8
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-02 16:19:02 +0900

    added sudo code

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -52,18 +52,24 @@ const renderCalendar = () => {
           days += `<div class="prev-date">${prevLastDay - prevDay + 1}</div>`;
         }
 
-
+        // Here we select the container we want to target
         const finished_workouts = document.querySelector(".calendar-container");
+        // Here we get the attribute we assigned in the html (which was to_json ed)
         finished_workouts.getAttribute('data-workouts')
+        // Here we use let so we can reassign, but we also parsed/translated the line above to javascript "language"
         let dates = JSON.parse(finished_workouts.getAttribute('data-workouts'))
+        // Here we map and assign a new "date" to turn the js date into a readable date and re-asign dates
         dates = dates.map(date => new Date(date))
+        // Here we filter just the workout dates that we are looking for
         dates = dates.filter((workout)  => {
-          if (
+          if ( //In these two lines we get and comparing the current date AND current month with workout date/month
             date.getMonth() == workout.getMonth() &&
             date.getYear() == workout.getYear()) {
+              // Make sure to return multiline statements
               return workout
             }
           })
+          //Here we re-asign dates again by getting the day and return the Date of that day
           dates = dates.map((date) => {
             return  date.getDate()
           })
@@ -76,6 +82,7 @@ const renderCalendar = () => {
               date.getMonth() === new Date().getMonth()
               ) {
                 days += `<div class="today">${day}</div>`;
+                // Here we get the the specific date of the day and check if its included in our "array"
               } else if (dates.includes(day)){
                 days += `<div class="worked-out-day">${day}</div>`;
               } else {
@@ -85,13 +92,6 @@ const renderCalendar = () => {
             // ______________________USE THIS TO PLOT GRAPH_______________________________
             // pass an array of dates to javascript (data attributes (maps -markers))
             // follow logic above - if the form calendar included in array of workouts, add class
-            // ___________________________________________________________________
-
-
-
-            console.log(dates)
-
-
             // ___________________________________________________________________
 
             for (let nextDay = 1; nextDay <= nextDays; nextDay++) {
