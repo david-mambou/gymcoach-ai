1d217c2d8f3a7358b83f73742236481b346f4dfa
David Mambou <david.mambou@gmail.com>
2021-12-02 12:05:23 +0900

    add david avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ goal1 = "Increase chest size"
 rtn = 'legs,chest,push'
 jesse = User.create!(email: 'jesse@lewagon.com', name: 'Jesse', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
 david = User.create!(email: 'david@lewagon.com', name: 'David', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
+david.profile_pic.attach(io: file, filename: 'david_profile.png', content_type: 'image/png')
 michael = User.create!(email: 'michael@lewagon.com', name: 'Michael', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
 renato = User.create!(email: 'renato@lewagon.com', name: 'Renato', password: psw, password_confirmation: psw, admin: true, goal: goal1, age: 30, routine: rtn)
 
