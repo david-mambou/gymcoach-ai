74ba1b0f3077a18418ed3e1f62dbfea859e3ec81
Michael Kubota <mchivas@mbm.nifty.com>
2021-12-03 13:32:12 +0900

    speech recognition/generation final testing on heroku

--- app/javascript/controllers/recognize_speech_controller.js
+++ app/javascript/controllers/recognize_speech_controller.js
@@ -34,22 +34,30 @@ export default class extends Controller {
     // Start recognising
     this.recognizer.onresult = function (event) {
       document.querySelector(".chat-input").value = '';
-      for (var i = event.resultIndex; i < event.results.length; i++) {
-        if (event.results[i].isFinal) {
-          document.querySelector(".chat-input").value = event.results[i][0].transcript;
-        } else {
-          document.querySelector(".chat-input").value += event.results[i][0].transcript;
-        }
-      }
+      document.querySelector(".chat-input").value = event.results[0][0].transcript;
+
+      // for (var i = event.resultIndex; i < event.results.length; i++) {
+      //   if (event.results[i].isFinal) {
+      //     document.querySelector(".chat-input").value = event.results[i][0].transcript;
+      //   } else {
+      //     document.querySelector(".chat-input").value += event.results[i][0].transcript;
+      //   }
+      // }
     };
     // Listen for errors
     this.recognizer.onerror = function (event) {
       console.log('Recognition error: ' + event.message);
     };
 
-    this.recognizer.onend = function (event) {
+    this.recognizer.onspeechend = function () {
       this.recognizer.stop();
-      console.log('Recognition stopped', event);
-    }
+      console.log('Recognition stopped');
+    };
+
+    this.recognizer.onsoundend = function () {
+      this.recognizer.stop();
+      console.log('Recognition stopped (soundend)');
+    };
+
   }
 }
