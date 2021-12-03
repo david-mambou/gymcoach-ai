80ab5cac2cedaa01c5b90c44c3becbf0b8806cac
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-03 12:59:24 +0900

    changed difficulty so it doesnt display very_________hard

--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -1,7 +1,7 @@
 class WorkoutSet < ApplicationRecord
   belongs_to :exercise
   belongs_to :workout
-  enum difficulty: { easy: 0, moderate: 1, hard: 2, very_hard: 3, failed: 4 }
+  enum difficulty: { "Easy" => 0, "Okay" => 1, "Hard" => 2, "Very Hard" => 3, "Failed" => 4 }
   validates :nb_of_reps, :weight, numericality: { greater_than_or_equal_to: 0 }
 
   # TODO: add current method
